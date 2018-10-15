17b0fc5ffb0ce8ca92b77877c29acdaa0955fe0f
fuck <fuckyou@fuck.fuck>
2018-10-15 23:55:16 +0200

    test

--- app/src/main/java/s/newgittest/someFile.java
+++ app/src/main/java/s/newgittest/someFile.java
@@ -4,5 +4,6 @@ public class someFile {
     public static void main(String[] args) {
         System.out.println("asdf2sdfsdf");
         System.out.println("new");
+        System.out.println("branch");
     }
 }
